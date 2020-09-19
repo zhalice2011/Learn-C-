56ce90d3045bb31f2881df7ccc577e580382210b
zhalice2011 <zhalice2012@gmail.com>
2020-09-20 07:48:56 +0800

    Initial Commit

---
+++ learn-c++-xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learn-c++-xcode
+//
+//  Created by 周达理 on 2020/9/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
