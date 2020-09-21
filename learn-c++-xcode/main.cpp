3c0a5af8e47387243eba39da977b91c8c340e07b
zhalice2011 <zhalice2012@gmail.com>
2020-09-21 17:54:12 +0800

    exercises 1.21

--- learn-c++-xcode/main.cpp
+++ learn-c++-xcode/main.cpp
@@ -6,9 +6,19 @@
 //
 
 #include <iostream>
+#include "Sales_item.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    Sales_item trans1, trans2;
+    
+    cout << "请输入两条ISBN相同的销售记录: " << endl;
+    
+    cin >> trans1 >> trans2;
+    
+    if (compareIsbn(trans1, trans2)) {
+        cout << "汇总信息" << trans1 + trans2 << endl;
+    }
+
     return 0;
 }
